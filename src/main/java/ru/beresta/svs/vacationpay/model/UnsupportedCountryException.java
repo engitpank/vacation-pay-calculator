1d7b12b73bf3df3ee4c4bb033832f3bc8c5a1b6e
engitpank <engutpunk@gmail.com>
2024-09-22 22:41:31 +0300

    refactor (model): 405 response for UnsupportedCountryException
    I think the status code 405: Method not Allowed is the best
    fit for your situation:
    status code indicates that the method received in the
    request-line is known by the origin server but not
    supported by the target resource.

--- src/main/java/ru/beresta/svs/vacationpay/model/UnsupportedCountryException.java
+++ src/main/java/ru/beresta/svs/vacationpay/model/UnsupportedCountryException.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 /**
  * The exception thrown when trying to execute action for an unsupported country.
  */
-@ResponseStatus(HttpStatus.BAD_REQUEST)
+@ResponseStatus(HttpStatus.METHOD_NOT_ALLOWED)
 public class UnsupportedCountryException extends IllegalArgumentException implements BusinessError {
     public UnsupportedCountryException(String message) {
         super(message);
